e7de7a3e10f39e8d361cdb501103145eb669d9cd
well016 <well016@mail.ru>
2024-10-26 17:13:45 +0300

    функция авторизации на сайте https://shelly.kpfu.ru/e-ksu/private_office.kfuscript

--- parcer.py
+++ parcer.py
@@ -1,29 +1,50 @@
+import fake_useragent
 import requests
 from bs4 import BeautifulSoup
-
+import re
+#Создаем сессию
+session = requests.Session()
+user = fake_useragent.UserAgent().random
+# Авторизация
 payload = {
     'p_login': 'RailRSabirov',
     'p_pass': 'QMT0Qk5mm7'
 }
-response = requests.post('https://shelly.kpfu.ru/e-ksu/private_office.kfuscript', data=payload)
-if "document.location.href" in response.text:
-    # Извлекаем URL для перенаправления
-    start = response.text.find("document.location.href='") + len("document.location.href='")
-    end = response.text.find("'", start)
-    redirect_url = response.text[start:end]
 
-    # Выполняем запрос на новый URL
-    new_response = requests.get(f'https://shelly.kpfu.ru/e-ksu/{redirect_url}')
-else:
-    print("Нет перенаправления или произошла ошибка")
-soup = BeautifulSoup(new_response.content, )
-print(soup)
-adres=soup.find_all('')
-print(adres)
+# Авторизация
+def auntification(login: str,password: str):
+    payload = {
+        'p_login': login,
+        'p_pass': password
+    }
+    headers = {
+        "User-Agent": user,
+    }
+    response = session.post('https://shelly.kpfu.ru/e-ksu/private_office.kfuscript', data=payload, headers=headers)
+    parsed_url = re.search(r"document\.location\.href='([^']+)'", response.text).group(1)
+    return parsed_url
+print(auntification(login='RailRSabirov',password='QMT0Qk5mm7'))
+
+def get_cookies():
+    pass
+page_account=session.get(f'https://shelly.kpfu.ru/e-ksu/{parsed_url}', headers=headers)
+
+pattern = r"setCookie\('([^']+)', '([^']+)'"
+
+# Поиск всех совпадений
+cookies = re.findall(pattern, page_account.text)
+
+# Преобразуем результат в словарь
+cookie_dict = {name: value for name, value in cookies}
+
+
+print(f'Вот куки {session.cookies}')
 
 
 
+print('Авторизация успешна' if response.ok else 'Авторизация не удалась')
 
 
+table=session.get('https://shelly.kpfu.ru/e-ksu/SITE_STUDENT_SH_PR_AC.shedule?p_menu=1', headers=headers, cookies=cookie_dict)
 
 
